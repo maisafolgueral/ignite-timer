8b920b5d8dd3a557e16b3f609b65dbfa157e6d5c
maisafolgueral <maisa.folgueral@gmail.com>
2024-06-21 11:34:45 -0300

    Added start new cycle

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,6 +13,7 @@ import
         StartCountdownButton, 
         TaskInput 
 } from "./styles";
+import { useState } from "react";
 
 const newCycleFormValidationSchema = zod.object({
     task: zod.string().min(1, 'Informe a tarefa'),
@@ -24,7 +25,16 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+    id: string;
+    task: string;
+    minutesAmount: number;
+}
+
 export function Home() {
+    const [cycles, setCycles] = useState<Cycle[]>([])
+    const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
     const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
         defaultValues: {
@@ -34,9 +44,21 @@ export function Home() {
     })
 
     function handleCreateNewCycle(data: NewCycleFormData) {
-        console.log(data)
+        const newCycle: Cycle = {
+            id: String(new Date().getTime()),
+            task: data.task,
+            minutesAmount: data.minutesAmount,
+        }
+
+        setCycles(state => [...state, newCycle]);
+        setActiveCycleId(newCycle.id)
+
         reset();
     }
+
+    const activeCycle = cycles.find(cycle => cycle.id == activeCycleId)
+
+    console.log(activeCycle)
 
     const task = watch('task')
     const isSubmitDisabled = !task
