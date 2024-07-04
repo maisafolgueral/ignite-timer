9c7233a9816908ee91bf6bc60a6bcddb4796927d
maisafolgueral <maisa.folgueral@gmail.com>
2024-07-03 21:06:18 -0300

    Updated route context

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -15,6 +15,7 @@ interface Cycle {
 }
 
 interface CyclesContextType {
+    cycles: Cycle[]
     activeCycle: Cycle | undefined;
     activeCycleId: string | null;
     amountSecondsPassed: number;
@@ -86,6 +87,7 @@ export function CyclesContextProvider({ children } : CyclesContextProviderProps)
     return (
         <CyclesContext.Provider 
             value={{ 
+                cycles,
                 activeCycle, 
                 activeCycleId, 
                 markCurrentCycleAsFinished, 
